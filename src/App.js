d22b261fec0f8fd358636fc61c4671f5c54c773f
sajustsmile <sajustsmile@live.com>
2022-03-07 22:57:26 +0100

    added padding to CollectionCard details div

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   <Header />
   <CollectionCard 
     id={0} 
-    name={'Bandana Punk'} 
+    name={'Coolio Punk'} 
     traits={[{'value': 7}]}
     image={'https://ipfs.thirdweb.com/ipfs/QmZ5fD3UTRh8ALZCpMdypHkhMQSXyi4yyCz3Ea19kPmtXg/0.jpg'}/>
     
--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -6,14 +6,15 @@ const CollectionCard = ({id, name, traits, image}) => {
   return (
     <div className='collectionCard'>
         <img src={image} alt="" />
-        <div className='details'></div>
+        <div className='details'>
         <div className='name'>
             {name} <div className='id'> •#{id}</div>
         </div>
         <div className='priceContainer'>
-            <img src={weth} alt="" />
+            <img className='wethImage' src={weth} alt="" />
             <div className='price'>{traits[0]?.value}</div>
         </div>
+    </div>
     </div>
   )
 }
